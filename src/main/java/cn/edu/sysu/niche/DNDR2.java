20f2f9bf57cb5eb0b72883300ea79c0831d3e7bb
songbeichang <songbeichang@163.com>
2021-08-25 18:56:38 +0800

    个体剔除操作初步完成

--- src/main/java/cn/edu/sysu/niche/DNDR2.java
+++ src/main/java/cn/edu/sysu/niche/DNDR2.java
@@ -104,29 +104,143 @@ public class DNDR2 {
 
     /**
      * 合并
-     *    集合调整
-     *    半径调整1
+     *    集合成员调整：取大leader做leader,List集合进行合并
+     *    半径调整1：选择大leader和小集群的最远值作为半径
      *    个体剔除操作
      *    半径调整2
      */
     public void merge(HashSet<String> hs) {
-        // 集合是否还需进一步去重,待进一步考量
+        // 初步合并
+        // 集合是否还需进一步去重,待考量
         for (String h : hs) {
-            mapArrayList.get(h.split(":")[0]);
-            mapArrayList.get(h.split(":")[1]);
-            System.out.println("++++++++++");
+            // leader
+            String s1 = h.split(":")[0];
+            String s2 = h.split(":")[1];
+            double l1 = Double.valueOf(s1.split("_")[1]);
+            double l2 = Double.valueOf(s2.split("_")[1]);
+            if(l1>l2){
+                // 集合成员调整
+                mapArrayList.get(s1).addAll(mapArrayList.get(s2));
+                // 需要将适应度小的种群剔除出leaderSet和mapArrayList
+                // mapArrayList.remove(s2)
+                // leaderSet.remove(s2)
+                // 半径调整1
+                radiusMerge(s1,s2,l1);
+            }else {
+                // 集合成员调整
+                mapArrayList.get(s2).addAll(mapArrayList.get(s1));
+                // 半径调整1
+                radiusMerge(s2,s1,l2);
+            }
         }
+
+
+        // 调整合并
+        // 通过hs来进行一次过滤,保留最新的mapArrayList集合
+        System.out.println("过滤前");
+        Iterator iter = mapArrayList.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Object key = entry.getKey();
+            Object val = entry.getValue();
+            System.out.println("key: "+key +"  val："+ val);
+        }
+
+        for (String h : hs) {
+            mapArrayList.remove(h.split(":")[0]);
+            mapArrayList.remove(h.split(":")[1]);
+        }
+
+        System.out.println("过滤后");
+        // HashMap遍历，此种方式效率高
+        Iterator iter2 = mapArrayList.entrySet().iterator();
+        while (iter2.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter2.next();
+            Object key = entry.getKey();
+            Object val = entry.getValue();
+            System.out.println("key: "+key +"  val："+ val);
+        }
+
+        // 个体剔除操作 + 半径调整2
+        // 调整的是各个小生境的半径,目的是为了可以剔除不需要的个体,以及获取峰半径,便于找到新个体后,在某个具体的小生境范围内进行GA
+        // 过滤出样式为value_key_radius的个体,只有这一部分个体半径需要做修改
+        // 用于存储临时集合
+        ArrayList<String> sideList = new ArrayList<>();
+
+        Iterator iter3 = mapArrayList.entrySet().iterator();
+        while (iter3.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter3.next();
+            String key = (String) entry.getKey();
+            Object val = entry.getValue();
+            if(key.split("_").length == 3){
+                // 用于存储集合
+                ArrayList<String> arrayList = new ArrayList<>();
+                // 进行剔除操作:(理论基础:小生境中适应度最低的个体 = 离领导个体最远的个体)
+                // 半径修改 ==> 如果yes,则保持原状即可。如果no,则将半径修改为最低的位置即可。
+                // 其会导致某些解不见,此时如何保留这些解呢？用一个新的集合others
+                // val：[0.9992600231455838_0.099, 0.9970430120324887_0.098,....]
+                String subStr = val.toString().substring(1, val.toString().length() - 1);
+                String[] splitArr = subStr.split(",");
+                // 寻找适应度值最低的个体 和 寻找适应度最高的个体
+                double lowerFitness = 9999;
+                double highFitness = 0;
+                String lowerIndividual = null;
+                String higherIndividual = null;
+                for (String s : splitArr) {
+                    Double tmpValue = Double.valueOf(s.split("_")[0]);
+                    if (tmpValue < lowerFitness){
+                        lowerFitness = tmpValue;
+                        lowerIndividual = s;
+                    }
+                    if (tmpValue > highFitness){
+                        highFitness = tmpValue;
+                        higherIndividual = s;
+                    }
+                }
+                
+                // 最终半径
+                double finalRadius = Double.valueOf(higherIndividual.split("_")[1]) - Double.valueOf(lowerIndividual.split("_")[1]);
+
+                // 根据距离判断是否需要剔除
+                for (String s : splitArr) {
+                    Double tmpValue = Double.valueOf(s.split("_")[1]);
+                    double distance =  tmpValue - Double.valueOf(lowerIndividual.split("_")[1]);
+                    // 如果距离大于半径，则需要将其剔除
+                    if (finalRadius < distance){
+                        // 剔除操作待完成  或者else重新设置一个集合,然后生成即可  侧输出流
+                        sideList.add(s);
+                    }else {
+                        arrayList.add(s);
+                    }
+                }
+                System.out.println(arrayList.size());
+                System.out.println(sideList.size());
+                System.out.println("==========");
+
+
+
+            }else{
+                // 暂时不进行格式的清洗value_key_radius,因为这将导致异常
+            }
+
+            // 更新key和value
+
+        }
+
+        // 通过mapArrayList 反过来修改 leaderSet,待验证是否有必要更新leaderSet
+
+
 
 
 
     }
 
 
+
     /**
      * 随机生成初代种群
      *      200个体  单基因
-     *      优化方案1：初始化一次，存入文件中，后续统一调用。但时间上估计不会相差太大。只是200个个体而已
-     *      优化方案2：初始化时保证0.001.这样可以方便后续的计算
+     *      优化方案：初始化一次，存入redis中,精度保证0.001.这样可以方便后续的计算
      *
      *      个体的越多,越具有代表性
      */
@@ -140,10 +254,11 @@ public class DNDR2 {
 
     /**
      * 初始化计算
+     *      1000/0.001= 100W的数据
      */
     private void  initSin() {
 
-        System.out.println("=== init  SIN_MAP ===");
+        System.out.println("=== init SIN_MAP ===");
         double step = 0.001;
         double end1 = 1000 ;
         for (double i = 0.000; i < end1; i=i+step) {
@@ -153,6 +268,7 @@ public class DNDR2 {
         for (double i = 0.000; i < end3; i=i+step) {
             SIN_MAP.put(numbCohesion(i),sin2(i));
         }*/
+
     }
 
 
@@ -351,40 +467,25 @@ public class DNDR2 {
 
     /**
      * 适应度排序
-     *  1.生成map(fitness,key)
-     *  2.排序
-     *
-     *  问题:
-     *      1.个体数存在!=200的情况吗？hashMap
-     *      2.降序排序已实现
-     *
-     *  目的:
      *      1.排序的目的是为了找出峰值
-     *      2.实现按照适应度降序,但希望能记住其原有的key,后续需使用key进行分组
-     *      3.map ×  list ×
-     *        3.1 list使用 value_key 进行排序,存在乱序现象,double进行比较即可
-     *              1.4655297809617463E-5_0.39
-     *              1.0_0.9
-     *        3.2 list使用 value 排序,再在后面需要用的时候,进行一次反向取值(反向取值可能存在难度,其也是hashMap)
+     *      2.按照适应度降序,但希望能记住其原有的key,后续需使用key进行分组
+     *      3.map ×  list √
+     *        list使用 (value_key) ,使用double进行比较解决乱序问题
      *
      *  最终结果:
      *      1.0_0.3
      *      0.9992600231455838_0.101
      *      ......
      *
-     *  如何存储呢?
-     *      1.直接将上述list(value_key)存起来
-     *      2.只存key
-     *
-     *
      */
     private void sortFitness() {
 
+        // 数组转list value_key
         for (double v : paper_genetic) {
             sortList.add(SIN_MAP.get(v)+"_"+v);
         }
 
-        Comparator comp = new Mycomparator();
+        Comparator comp = new MyComparator();
         Collections.sort(sortList,comp);
 
         /*for(int i = 0;i<sortList.size();i++){
@@ -403,8 +504,6 @@ public class DNDR2 {
 
     /**
      * 使用 Map按key进行排序
-     * @param map
-     * @return
      */
     public static Map<Double, Double> sortMapByKey(Map<Double, Double> map) {
         if (map == null || map.isEmpty()) {
@@ -425,7 +524,7 @@ public class DNDR2 {
      *
      *  后续步骤：
      *      1.选取leader
-     *           最后有些个体其实不应该成为leader，暂时不深究，待后续完成
+     *           最后有些个体其实不应该成为leader，待后续进行合并和剔除操作
      *      2.选取member
      *           保证数据总数不变
      */
@@ -447,7 +546,7 @@ public class DNDR2 {
                         counter = counter + 1;
                     }
                 }
-
+                // 如果隶属于一个小生境
                 if(counter < 1){
                     leaderSet.add(sortList.get(i));
                     System.out.println("leader: "+sortList.get(i));
@@ -455,11 +554,12 @@ public class DNDR2 {
             }
         }
 
-        // 选取member ①每选完一个leader之后立即进行选取，可以避免无效的迭代 ②选完全部leader之后再进行，可以避免ConcurrentModificationException
-        // 是否存在一个点分布在两个小生境中  no
-        //     现象：存在一个值在两个小生境中   0.4 分别在0.301和0.0407中
-        //     解决方案：每次判断完后，对这个个体标记，表明已经清除
         // 选取member
+        //      ①每选完一个leader之后立即进行选取，可以避免无效的迭代
+        //      ②选完全部leader之后再进行，可以避免ConcurrentModificationException
+        //
+        //     现象：存一个点分布在两个小生境中   0.4 分别在0.301和0.0407中
+        //     解决方案：每次判断完后，对这个个体标记，表明已经清除
         int sum = 0;
         for (String leader : leaderSet) {
             ArrayList<String> memberList = new ArrayList<>();
@@ -478,20 +578,18 @@ public class DNDR2 {
             sum = memberList.size() + sum;
         }
 
-
-        System.out.println(sum);
-
+        System.out.println("此次迭代个体总数目："+sum);
 
     }
 
     /**
      * 判断哪些峰需要合并(矩阵+凹点问题) mapArrayList leaderSet
-     *    用距离公式来表示任意两个小生境之间的关系,得出一个距离关系的w矩阵,因为是矩阵,所以后续需进一步去重
+     *    用距离公式来表示任意两个小生境之间的关系,得出一个距离关系w矩阵,因为是矩阵,所以后续需进一步去重
      *
      *    使用什么来保存矩阵关系呢？
-     *    此方法返回一个动态大小的二维数组，而不是使用全局变量
+     *    此方法一个动态大小的二维数组，而不是使用全局变量
      *
-     *    待优化：其实没必要返回距离关系矩阵，只要将要合并的集合返回即可，可能是一个，也可能是多个
+     *    优化：对返回距离关系矩阵，只要将要合并的集合返回即可，可能是一个或多个
      *
      *
      */
@@ -505,19 +603,19 @@ public class DNDR2 {
             }
         }*/
 
-        //System.out.println(leaderSet.size()+" vs "+nl.size());
-
         // 距离关系w矩阵
         int[][] distanceMatrix =new int[leaderSet.size()][leaderSet.size()];
 
         // set 转 arrayList
         List<String> leaderList = new ArrayList<>(leaderSet);
-        //System.out.println(leaderList);
 
         // 遍历获取距离关系,并生成矩阵
         for (int i=0;i < leaderList.size(); i++) {
+
             double min = 9999; int a=0; int b=0;
+
             Double aDouble = Double.valueOf(leaderList.get(i).split("_")[1]);
+
             for (int j=0;j < leaderList.size(); j++) {
 
                 if (!leaderList.get(i).equals(leaderList.get(j))){
@@ -531,13 +629,10 @@ public class DNDR2 {
             }
 
 
-            // 随机选取5个点进行凹点验证
-            Double bDouble = Double.valueOf(leaderList.get(b).split("_")[1]);
-            //System.out.println(aDouble +","+ bDouble);
-
+            // 随机选取5个点进行凹点验证：为了解决独立因子
             // 多个随机值当中,只要存在凹点,证明这两个相邻的小生境是独立的,不需要合并
             // 为了消除噪音干扰，定义了一个忍受因子 sf=0.9
-            // 凹点问题就是为了解决独立因子吗？  yes
+            Double bDouble = Double.valueOf(leaderList.get(b).split("_")[1]);
             double sf = 0.9;
 
             // 一次循环均为正常,则需要合并
@@ -545,8 +640,7 @@ public class DNDR2 {
             for (int j = 0; j < 5; j++) {
                 double lemuda = Math.random();
                 double cDouble = (lemuda * aDouble) + ((1 - lemuda) * bDouble);
-                //System.out.println(cDouble);
-                // 计算适应度值,并进行比较  判断逻辑存在问题吗？
+                // 计算适应度值,并进行比较
                 Double aFitness = SIN_MAP.get(numbCohesion(aDouble));
                 Double bFitness = SIN_MAP.get(numbCohesion(bDouble));
                 Double cFitness = SIN_MAP.get(numbCohesion(cDouble));
@@ -565,7 +659,6 @@ public class DNDR2 {
             if(flag){
                 // 将ab有值时,赋值为1,其余赋值为0
                 distanceMatrix[a][b]=1;
-
             }
         }
 
@@ -598,16 +691,12 @@ public class DNDR2 {
         HashSet<String> hs = new HashSet<>();
         for (int i1 = 0; i1 < distanceMatrix.length; i1++) {
             for (int i2 = 0; i2 < distanceMatrix.length; i2++) {
-                // 通过对ab排序,将小数字放在前面,然后使用set,就解决了重复的问题
+                // 通过对ab排序,将小数字放在前面,然后使用set,解决重复的问题
                 // 合并去重的情况也需要进一步考虑 [0.798_0.9, 0.696_0.798] 即 0,2 和 2,5 待进一步考量
                 if(distanceMatrix[i1][i2] == 1){
                     if(i1<i2){
-                        //System.out.println(i1+","+i2);
-                        //hs.add(leaderList.get(i1).split("_")[1]+"_"+leaderList.get(i2).split("_")[1]);
                         hs.add(leaderList.get(i1)+":"+leaderList.get(i2));
                     }else {
-                        //System.out.println(i2+","+i1);
-                        //hs.add(leaderList.get(i2).split("_")[1]+"_"+leaderList.get(i1).split("_")[1]);
                         hs.add(leaderList.get(i2)+":"+leaderList.get(i1));
                     }
                 }
@@ -617,6 +706,46 @@ public class DNDR2 {
         System.out.println(hs.toString());
         return hs;
     }
+
+
+    /**
+     * 半径合并
+     *      s1是高峰,s2是低峰
+     *      集合可以改变,但不能改变leaderSet,因为会影响后续的合并操作
+     */
+    private void radiusMerge(String s1,String s2,double l1) {
+
+        // 进行迭代,选择最远的个体即可,寻找最大距离，即为新半径,
+        double maxDiff = 0;
+        for (String s : mapArrayList.get(s2)) {
+            //System.out.println(s);
+            double tmpDistance = Double.valueOf(s.split("_")[1]);
+            double tmpDiff = Math.abs(tmpDistance - l1);
+            if(maxDiff < tmpDiff){
+                maxDiff = tmpDiff;
+            }
+        }
+        // 需要使用某种方式将新半径保存下来,改变其数据结构 value_key_radius
+        // map可以修改key吗？还是说使用一个新的map进行存储
+        /**
+         * map是无法直接修改key值的，所以要采用其他的方案，新增一个键值对，再删除之前那个要修改的
+         * map要使用ConcurrentHashMap，不然会抛.ConcurrentModificationException异常，然后采用迭代器的方式遍历，不能用for循环，不然也会抛ConcurrentModificationException异常，然后在迭代中it.remove(),map.put();就可以了
+         *
+         */
+        String s = s1 + "_" + maxDiff;
+        mapArrayList.put(s, mapArrayList.get(s1));
+        // 峰的移除，是否会有其他影响。
+        //    移除目的：为了下一步的小生境剔除操作提供了便利
+        //    保留目的：解决合并的过程中存在一个集合多次合并,若提前移除,导致空值现象
+        // java.lang.NullPointerException
+        // [0.8983199482630352_0.512:4.328701998830273E-5_0.412, 4.328701998830273E-5_0.412:0.9992600231455838_0.301]
+        //mapArrayList.remove(s1);
+        //mapArrayList.remove(s2);
+        // 现象：个体数小于200，待核实
+        //System.out.println(mapArrayList);
+
+    }
+
 
 
 
@@ -636,7 +765,7 @@ class MapKeyComparator implements Comparator<Double>{
     }
 }
 
-class Mycomparator implements Comparator{
+class MyComparator implements Comparator{
     @Override
     public int compare(Object str1, Object str2) {
         return  Double.valueOf(str2.toString().split("_")[0]).compareTo(Double.valueOf(str1.toString().split("_")[0]));
